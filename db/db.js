056ab33b020cc211482fad815ee757764c19a50a
e-mbrown <ebsonari@gmail.com>
2021-03-26 12:18:38 -0400

    Fixed db connection, added app to server, started blog routes

--- db/db.js
+++ db/db.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 const pgp = require("pg-promise")()
 
 const cn = {
-
+    host: 'localhost',
+    //default  port
+    port: 5432,
     database: 'blogtest',
     user: process.env.USER,
     password: process.env.PASSWORD
@@ -10,6 +12,22 @@ const cn = {
 
 const db = pgp(cn)
 
-module.exports = {
-    db
-}
+db.connect()
+    .then(obj => {
+        //saves connection object
+        sco = obj
+        return sco.one('SELECT * FROM users LIMIT 1')
+    })
+    .then(data =>{
+        console.log(data.id, 'connection tested')
+    })
+    .catch( err => {
+        console.log(`There is an issue with the db. Error: ${err}`)
+    })
+    .finally(() =>{
+        if(sco){
+            console.log('Db fully operational')
+            sco.done
+        }
+    })
+module.exports = db
