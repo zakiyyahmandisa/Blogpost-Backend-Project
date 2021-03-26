32e56b02f16c70a3dbb6cb115ffc59c4d0a4ba26
e-mbrown <ebsonari@gmail.com>
2021-03-26 19:33:18 -0400

    finished get on tags

--- controllers/blog.js
+++ controllers/blog.js
@@ -11,12 +11,13 @@ const getBlog = async (req, res) => {
     }
 };
 
-const createBlog = async (req,res) => {
+const createBlog = async (req,res,next) => {
     try{
         const date = new Date()
         await db.none('INSERT INTO blogposts (post,date,user_id) VALUES ($1,$2,$3)', [req.body.post, date, req.body.user_id])
         const data = await db.one('SELECT * FROM blogposts WHERE user_id=$1 AND date=$2', [req.body.user_id, date])
-        res.status(200).json({message:'Post created', data:data})
+        // res.status(200).json({message:'Post created', data:data})
+        next()
     }catch(err){
         console.log(err)
         res.status(400).send('error occured, did not update')
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require("express");
+const { createTag, checkTag } = require('./controllers/tags')
 const { getBlog, createBlog, updateBlog, deleteBlog } = require('./controllers/blog')
 const { createUsers} = require('./controllers/user')
 const app = express()
@@ -7,15 +8,21 @@ const app = express()
 app.use(express.json())
 
 const PORT = process.env.PORT || 3000;
-//creates a new blog post 
-//display all blog post 
+
 app.get("/blogs", getBlog)
-app.post('/blogs', createBlog)
+app.post('/blogs',createBlog, checkTag)
+//id is blog id not user id
 app.put("/blogs/revise/:id", updateBlog)
 app.delete("/blogs/:id", deleteBlog)
+
+app.post('/tags',createTag)
+
 // app.post('/user', createUsers)
-//updates a blog post
-//deletes a blog post 
+
+//flow
+    //create blog
+    //Check tags on blogs, if the exist finish check make connection on relation table
+    //if they dont exist make them and connect on relation table
 
 
 
