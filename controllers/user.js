6796deafbc998ed6d12b9fdc5d26a349c093df31
Zakiyyah <zakiyyahmandisa@gmail.com>
2021-03-26 13:06:42 -0400

    user routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,21 +1,24 @@
 const db = require('../db/db')
 
-
+//get all created users 
 const getUser = async(req,res) => {
-    try{
+    try{ 
+        const users = await db.many(`SELECT * FROM users`)
 
     }
     catch(err){
+        res.status(500).send(err)
 
     }
 }
-
+//create one user 
 const createUser = async (req,res) =>{
-    try{
-        
+    try{ const user = await db.one (`INSERT INTO users (fname, username, password, profile_pic) VALUES (${fname}, ${username}, ${password}, ${profile_pic})`)
+      req.body  
 
     }
     catch(err){
+        res.status(500).send(err)
 
     }
 }
